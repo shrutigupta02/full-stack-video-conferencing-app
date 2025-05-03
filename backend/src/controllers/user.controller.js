0250c1443cf7dde0bec66101067996e734172422
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2025-05-03 21:11:58 +0530

    Added login and register controllers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,19 +1,67 @@
-import { User } from "../models/user.model"
-import httpStatus from "http-status"
-import bcrypt from "bcrypt"
+import httpStatus from "http-status";
+import { User } from "../models/user.model.js";
+import bcrypt, { hash } from "bcrypt"
 
-const register = async (req, res) => {
-    const {name, username, password} = req.body
+import crypto from "crypto"
+const login = async (req, res) => {
 
-    try{
-        const existingUser = User.findOne({username})
+    const { username, password } = req.body;
 
-        if(existingUser){
-            return res.status(httpStatus.FOUND).json({message: "User already exists"})
+    if (!username || !password) {
+        return res.status(400).json({ message: "Please Provide" })
+    }
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" })
         }
 
-        const hashedPassword = await bcrypt.hash(password)
-    }catch{
 
+        let isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if (isPasswordCorrect) {
+            let token = crypto.randomBytes(20).toString("hex");
+
+            user.token = token;
+            await user.save();
+            return res.status(httpStatus.OK).json({ token: token })
+        } else {
+            return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Username or password" })
+        }
+
+    } catch (e) {
+        return res.status(500).json({ message: `Something went wrong ${e}` })
     }
 }
+
+
+const register = async (req, res) => {
+    const { name, username, password } = req.body;
+
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(httpStatus.FOUND).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name: name,
+            username: username,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        res.status(httpStatus.CREATED).json({ message: "User Registered" })
+
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+
+}
+
+export { login, register }
