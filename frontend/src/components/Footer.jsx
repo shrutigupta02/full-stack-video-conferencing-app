7621bd21c2c7cbb3fc75defa77ae110046fdcee9
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2025-05-08 18:07:32 +0530

    Landing Page
    Styling and creation of landing page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p>&copy; 2025 Shruti Gupta. All rights reserved. <br/>
+            Know more about the developer: <a href="https://shrutidev.netlify.app" target="_blank">Click here</a> <br/>
+            Work enquiry only: <a href="mailto:shrutiguptaindore02@gmail.com">shrutiguptaindore02@gmail.com</a></p>
+        </div>
+    )
+}
